19b402c017c037410209c8630cbc52dc255ef6f3
Marcus Palombello <marcus@onbrandhq.com>
2021-04-23 18:57:46 +0100

    feat: added cocktail index method. Fix eslint

--- src/api/services/resources/cocktails.js
+++ src/api/services/resources/cocktails.js
@@ -4,6 +4,26 @@
  * ```
  */
 
-const CocktailsService = {};
+import axios from "@/plugins/axios.js";
+
+const apiUrl = process.env.VUE_APP_API_URL;
+
+const CocktailsService = {
+  /**
+   * It returns cocktails data
+   *
+   * @param {object} queryParams Request parameters
+   * @returns {Promise} Cocktail promise
+   */
+  index(queryParams = {}) {
+    return axios
+      .get(`${apiUrl}cocktails`, {
+        params: queryParams,
+      })
+      .then((response) => {
+        return response.data;
+      });
+  },
+};
 
 export default CocktailsService;
