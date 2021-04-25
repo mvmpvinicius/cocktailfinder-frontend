618fd9d75aa97e8fce3f2873a3f0b7a3dc569d51
Marcus Palombello <marcus@onbrandhq.com>
2021-04-25 17:18:22 +0100

    feat: Added ingredients cocktail card

--- src/components/cocktails-list-component/CocktailsListComponent.js
+++ src/components/cocktails-list-component/CocktailsListComponent.js
@@ -19,8 +19,34 @@ export default {
   },
 
   methods: {
+    /**
+     * It gets formatted ingredient from a cocktail
+     *
+     * @param {object} cocktail Cocktail object to collect ingredients
+     * @returns {Array} Array of ingredients with measure
+     */
     buildUpIngredients(cocktail) {
-      console.log("cocktail", cocktail);
+      let ingredients = [];
+
+      let stop = false;
+
+      for (let i = 1; stop != true; i++) {
+        let strIng = "strIngredient" + i;
+        let strMea = "strMeasure" + i;
+
+        if (cocktail[strIng]) {
+          let text =
+            cocktail[strIng] +
+            (cocktail[strMea] ? " / " + cocktail[strMea] : "");
+          ingredients.push(text);
+        }
+
+        if (!cocktail[strIng]) {
+          stop = true;
+        }
+      }
+
+      return ingredients;
     },
   },
 };
